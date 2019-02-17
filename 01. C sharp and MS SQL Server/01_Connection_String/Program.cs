a71650a43756014cf0d208b1859bf41f0d6f9cb8
levonaramyan <levonaramyan@gmail.com>
2019-02-17 15:46:12 +0400

    New: Connection String

---
+++ 01. C sharp and MS SQL Server/01_Connection_String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration; // for using App.config file for getting connection string
+                            // necessary to add into references System.Configuration.dll
+
+namespace _01_Connection_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initializing the connection string
+            string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Astro;Integrated Security=True;";
+
+            // Best Practice: getting the connection string from configuration manager
+            string connStr2 = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            Console.WriteLine(connStr+"\n"+connStr2); // getting sure that the strings are equal
+
+            Console.ReadKey();
+        }
+    }
+}
